970c3b1b77ef53d789aab5eba310302b1642514a
Ehmaed Alhams <ehmaedalzhar@gmail.com>
2022-10-08 18:08:15 +0300

    update: section stats, fill stats.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
 let btn = document.querySelector("button");
 let skills = document.querySelector(".skills");
 let progress = document.querySelectorAll('.progr-bars span');
+let nums = document.querySelectorAll(".stats .col .num");
+let stats = document.querySelector('.stats');
 
 onScrollEvent();
 handleUpButtonClick();
 
-
+let start = false;
 function onScrollEvent() {
     window.onscroll = function () {
         showUpButton();
         fillProgress();
+        fillStats();
     }
     // window.addEventListener('scroll',function () {
-
     // });
 }
 
 function showUpButton() {
-        if (window.scrollY >= 500) {
-            btn.style.display = "block";
-        } else {
-            btn.style.display = "none";
-        }
+    if (window.scrollY >= 500) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
 }
 
 function handleUpButtonClick() {
@@ -36,10 +38,32 @@ function handleUpButtonClick() {
 
 
 function fillProgress() {
-        if (window.scrollY >= skills.offsetTop - 250) {
-            progress.forEach((progress) => { 
-                progress.style.width = progress.dataset.progress;
-            });
-        }
+    if (window.scrollY >= skills.offsetTop - 250) {
+        progress.forEach((progress) => { 
+            progress.style.width = progress.dataset.progress;
+        });
+    }
 }
 
+function handleStats(element) {
+    let goal = element.dataset.goal;
+    let count = setInterval(function () { 
+        element.textContent++;
+        if (element.textContent == goal) {
+            clearInterval(count);
+        };
+    }, 5000 / goal);
+}
+
+
+
+function fillStats() {
+    if (!start) {
+        if (window.scrollY >= stats.offsetTop - 250) {
+            nums.forEach((num) => { 
+                handleStats(num);
+            });
+            start = true;
+        };
+    }
+}
